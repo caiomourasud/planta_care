7c895ce7d1930d1025ddbee948e4c16c8ed4ef37
Caio Moura <caiomourasud@gmail.com>
2025-02-27 00:01:25 -0300

    Adding font Urbanist

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,20 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'Urbanist',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
